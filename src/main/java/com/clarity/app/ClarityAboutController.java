3a1a1ed74e10e90b5cd6b8cb09b507312a41f8e1
Jhonmark Tecson Lumacang <jlumacang91235@liceo.edu.ph>
2025-11-16 22:48:39 +0800

    added new file ClarityAboutController.java

---
+++ src/main/java/com/clarity/app/ClarityAboutController.java
@@ -0,0 +1,11 @@
+package com.clarity.app;
+
+import javafx.fxml.FXML;
+
+public class ClarityAboutController extends BaseController {
+
+    @Override
+    public void initialize() {
+        System.out.println("ClarityAboutController initialized");
+    }
+}
